4db14bd4f860d87dd0b7ce4e34a35d0f72eba553
Yuan Wang <yuan.wang@workiva.com>
2018-04-26 12:44:46 -0600

    add a property test case

--- tr/test/Spec.hs
+++ tr/test/Spec.hs
@@ -6,36 +6,46 @@
 -- Run the tests with `stack test`.
 module Main (main) where
 
-    import Test.Hspec
-    import Test.QuickCheck
+import           Test.Hspec
+import           Test.QuickCheck
 
-    import Tr
+import           Tr
 
-    type CharSet' = NonEmptyList Char
+type CharSet' = NonEmptyList Char
 
-    tr' :: CharSet -> CharSet -> String -> String
-    tr' inset outset = tr inset (Just outset)
+tr' :: CharSet -> CharSet -> String -> String
+tr' inset outset = tr inset (Just outset)
 
-    -- | Test harness.
-    main :: IO ()
-    main = hspec $ describe "Testing tr" $ do
-        describe "single translate" $
-          it "a -> b" $
-            tr' "a" "b" "a" `shouldBe` "b"
+deleteMode :: CharSet -> String -> String
+deleteMode inset = tr inset Nothing
 
-        describe "stream translate" $
-          it "a -> b" $
-            tr' "a" "b" "aaaa" `shouldBe` "bbbb"
+-- | Test harness.
+main :: IO ()
+main = hspec $ describe "Testing tr" $ do
+  describe "single translate" $
+    it "a -> b" $
+      tr' "a" "b" "a" `shouldBe` "b"
 
-        describe "extend input set" $
-          it "abc -> d" $
-            tr' "abc" "d" "abcd" `shouldBe` "dddd"
+  describe "stream translate" $
+    it "a -> b" $
+      tr' "a" "b" "aaaa" `shouldBe` "bbbb"
 
-        describe "tr quick-check" $
-          it "empty input is identity" $ property prop_empty_id
+  describe "extend input set" $
+    it "abc -> d" $
+      tr' "abc" "d" "abcd" `shouldBe` "dddd"
 
-    -- | An example QuickCheck test. Tests the invariant that `tr` with an empty
-    -- input string should produce and empty output string.
-    prop_empty_id :: CharSet' -> CharSet' -> Bool
-    prop_empty_id (NonEmpty set1) (NonEmpty set2)
-      = tr' set1 set2 "" == ""
+  describe "tr quick-check" $
+    it "empty input is identity" $ property prop_empty_id
+
+  describe "tr quick-check 2" $
+    it "delete itself returns empty result" $ property prop_delete_itself
+
+
+-- | An example QuickCheck test. Tests the invariant that `tr` with an empty
+-- input string should produce and empty output string.
+prop_empty_id :: CharSet' -> CharSet' -> Bool
+prop_empty_id (NonEmpty set1) (NonEmpty set2)
+  = tr' set1 set2 "" == ""
+
+prop_delete_itself :: CharSet' -> Bool
+prop_delete_itself (NonEmpty set1) = deleteMode set1 set1 == ""
