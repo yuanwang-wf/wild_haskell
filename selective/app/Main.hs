b3d3519b4a5788b809ec45c542169adcaf35e825
Yuan Wang <yuan.wang@workiva.com>
2019-03-06 12:32:32 -0600

    WIP

--- selective/app/Main.hs
+++ selective/app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
-import Lib
+import Control.Applicative (liftA3)
+
+pingPongM :: IO ()
+pingPongM = getLine >>= (\ s -> if s == "ping" then putStrLn "pong" else return ())
+-- getLine >>= Control.Monad.when( s == "ping") $ putStrLn "pong"
+
+pingPongA :: IO ()
+pingPongA =  (\ s -> id) <$> getLine <*> putStrLn "pong"
+
+pingPongIfElseA :: IO ()
+pingPongIfElseA =  liftA3 (\ s a b -> if s == "ping" then a else b )  getLine  (putStrLn "pong") (pure ())
 
 main :: IO ()
-main = someFunc
+main = pingPongM
